46667f8d234c6f26157aab7f119761e91d17d2f1
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-02-03 13:12:54 -0700

    Add closing_time method; test passing.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,4 +8,8 @@ class Restaurant
     @opening_time = opening_time
     @dishes = []
   end
+
+  def closing_time(hours_of_operation)
+    (@opening_time.to_i + hours_of_operation).to_s + ":00"
+  end
 end
