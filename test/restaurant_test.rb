4ffde7f19227a11c2695db021152280febe4fa08
Jennifer Brabson <jenniferbrabson@Jennifers-MacBook-Pro.local>
2021-02-03 13:27:16 -0700

    Add open_for_lunch? method; tests passing.

--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -44,7 +44,9 @@ class RestaurantTest < Minitest::Test
   end
 
   def test_it_is_open_for_lunch
-
-
+    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+    assert_equal true, restaurant1.open_for_lunch?
+    assert_equal false, restaurant2.open_for_lunch?
   end
 end
